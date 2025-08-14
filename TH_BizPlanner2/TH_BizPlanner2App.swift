4a0ff2f1e5de53ba4ca7bc68bbb9b6bd8880bf20
DJUROM777 <djurom777@ya.ru>
2025-08-14 20:26:12 +0300

    Initial Commit

---
+++ TH_BizPlanner2/TH_BizPlanner2App.swift
@@ -0,0 +1,17 @@
+//
+//  TH_BizPlanner2App.swift
+//  TH_BizPlanner2
+//
+//  Created by IGOR on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct TH_BizPlanner2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TH_BizPlanner2Tests/TH_BizPlanner2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  TH_BizPlanner2Tests.swift
+//  TH_BizPlanner2Tests
+//
+//  Created by IGOR on 14/08/2025.
+//
+
+import Testing
+
+struct TH_BizPlanner2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
